4688b33695178c16471e7fc936428dd41213c6ed
Sesar Weldi <sesar@happy5.co>
2024-06-22 19:57:04 +0700

    config: initialize autoroute

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:klob_kb_apps/pages/home/home_page.dart';
+part 'app_router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(
+          page: HomeRoute.page,
+          path: '/',
+        ),
+      ];
+}
